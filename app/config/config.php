5564b506a8fe9a336a2419d35b6759f8eafbd78e
Emad Albouni <emadbrown2013@gmail.com>
2020-11-04 10:40:12 +0100

    First commit installation files

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'texas-scramble');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/texas-scramble/');
+  // Site Name
+  define('SITENAME', 'texas-scramble');
---
+++ app/views/_include/head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <link rel="icon" type="image/png" href="<?php echo URLROOT; ?>img/logo.png">
+    </head>
+    <body>
+            <div class="container-fluid">
