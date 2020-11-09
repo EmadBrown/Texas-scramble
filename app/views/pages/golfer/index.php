bd48d0d89de06c25b040a41f6cf15c3e01a7fa0c
Emad Albouni <emadbrown2013@gmail.com>
2020-11-09 19:05:15 +0100

    Golfer index view

--- app/views/pages/golfer/index.php
+++ app/views/pages/golfer/index.php
@@ -1,4 +1,33 @@
 <?php
 
+?>
+    <div class="container-fluid">
+        <div class="row">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Geslacht</th>
+                    <th scope="col">Handicap</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach ($data['golfers'] as $key => $golfer) {
 
-var_dump($data['golfers']);
+                    $gender = $golfer->gender == 'M' ? "Man" : "Vrouw";
+                    echo "<tr>
+                            <th scope='row'>$golfer->golfer_id</th>
+                            <td>$golfer->name</td>   
+                            <td>$gender</td>
+                            <td>$golfer->handicap</td>
+                        </tr>";
+
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+<?php
