0f09ee6a51b143a88e980de15bfeb3ff163d4cd7
Emad Albouni <emadbrown2013@gmail.com>
2020-11-09 16:59:11 +0100

    Curd Golfer / create view

--- app/controllers/GolferController.php
+++ app/controllers/GolferController.php
@@ -4,8 +4,77 @@
 class GolferController extends Controller
 {
 
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Display a listing Golfers
+     *
+     */
+
     public function index()
     {
         $this->view('pages/golfer/index', ['title' => 'Golfer']);
+    }
+
+
+    /**
+     * Show the form for creating a new Golfer.
+     *
+     */
+    public function create()
+    {
+        //
+        return $this->view('pages/golfer/create');
+
+    }
+
+    /**
+     * Store a newly created Golfer in storage.
+     *
+     */
+    public function store()
+    {
+
+    }
+
+    /**
+     * Display the specified Golfer.
+     *
+     * @param int $id
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified Golfer.
+     *
+     * @param int $id
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified Golfer in storage.
+     *
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified Golfer from storage.
+     *
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
