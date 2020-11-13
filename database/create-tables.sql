30be34c3e799aabb4558f8c489a2248f10c81956
Emad Albouni <emadbrown2013@gmail.com>
2020-11-13 03:27:55 +0100

    fixed databse script create & insert

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -1,30 +1,37 @@
+
+
 CREATE TABLE `golfers` (
   `golfer_id` integer unique AUTO_INCREMENT,
+  `flight_id` integer default null,
   `name` varchar(50)  not null,
   `handicap` double not null default '0.00',
-  `gender` enum('M', 'F') not null,
+  `gender` enum('male', 'female') not null,
   `create_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
-  KEY `pk` (`golfer_id`)
+  KEY `pk` (`golfer_id`),
+  KEY `fk` (`flight_id`)
 );
 
-CREATE TABLE `tournaments` (
+CREATE TABLE ` tournaments` (
   `tournament_id` integer unique AUTO_INCREMENT,
-  `flight_id` integer not null,
   `create_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
-  KEY `pk` (`tournament_id`),
-  KEY `fk` (`flight_id`)
+  KEY `pk` (`tournament_id`)
 );
 
 CREATE TABLE `flights` (
   `flight_id` integer unique AUTO_INCREMENT,
-  `golfer_id` integer not null,
+  `tournament_id` integer default null,
   `num_of_players` integer,
   `type` varchar(50),
   `create_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
   KEY `pk` (`flight_id`),
-  KEY `fk` (`golfer_id`)
+  KEY `fk` (`tournament_id`)
 );
+
+
+
+
+
 
